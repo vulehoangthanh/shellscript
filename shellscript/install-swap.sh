89a4edf50660832c34b42bcc415879f986604c74
Vu Le <vu.lehoangthanh16051991@gmail.com>
2021-05-22 14:58:51 +0700

    Update install-swap.sh

--- shellscript/install-swap.sh
+++ shellscript/install-swap.sh
@@ -4,3 +4,10 @@ sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 free -h
+#Make the Swap File Permanent
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo 'vm.swappiness=10' >> /etc/sysctl.conf
+sudo sysctl vm.vfs_cache_pressure=50
+echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
